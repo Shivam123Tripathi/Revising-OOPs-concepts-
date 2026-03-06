bb29464ecfb24fa44daea94eb86376c94c90fd7b
Shivam Tripathi <s.shivamtripathi@gmail.com>
2026-03-06 10:45:18 +0530

    commiting initial commits:class

---
+++ src/main/java/classAndObjects.java
@@ -0,0 +1,15 @@
+public class classAndObjects {
+    class Car{
+        String brand;
+        int speed;
+
+        void  drive(){
+            System.out.println(brand + "is driving at " + speed + "km/h");
+        }
+    }
+}
+
+//here we created blueprint called car(this is class)
+//car will have brand(brand property)
+//car will have speed(speed property)
+//car will have behaviour (function drive() )
