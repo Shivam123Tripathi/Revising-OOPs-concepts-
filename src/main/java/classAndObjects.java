fb4674c18064c30600f15751e3a4daf015ce4626
Shivam Tripathi <s.shivamtripathi@gmail.com>
2026-03-06 10:52:27 +0530

    created car object

--- src/main/java/classAndObjects.java
+++ src/main/java/classAndObjects.java
@@ -4,11 +4,28 @@ class Car{
 
         void  drive(){
             System.out.println(brand + "is driving at " + speed + "km/h");
+
+            //here we created blueprint called car(this is class)
+            //car will have brand(brand property)
+            //car will have speed(speed property)
+            //car will have behaviour (function drive() )
+
+        }
+        public class Main{
+            public static void main(String[] args) {
+                Car c = new Car();//obj is created (in heap memory)
+                c.brand = "Toyota";
+                c.speed = 100;
+
+                c.drive();//Toyota is driving at 100 km/h
+
+                //now from blueprint of car we have created real car instance.
+                //in heap memory
+                //an object a real instance of a class
+
+            }
         }
     }
 
 
-//here we created blueprint called car(this is class)
-//car will have brand(brand property)
-//car will have speed(speed property)
-//car will have behaviour (function drive() )
+
